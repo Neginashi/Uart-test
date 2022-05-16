ccc5a0d7231b1fc51a5911973ca156719e77e864
Neginashi <52062701+Neginashi@users.noreply.github.com>
2022-05-16 18:25:27 +0900

    Update UART.v

--- UART.v
+++ UART.v
@@ -9,8 +9,7 @@ input	RST;
 
 input	reg	[119:0]		PC_DATA;
 input	reg	[63:0]		R_DATA;
-
-output	reg [15:0]		
+	
 output	reg	[63:0]		DSP_DATA;
 output	reg	[39:0]		DSP;
 
@@ -20,9 +19,9 @@ wire	[23:0]	W_ACTION;
 wire	[87:0]	BEFORE_R_ADDR;
 wire	[23:0]	R_ACTION;
 wire	[31:0]	W_ADDR;
-wire	[31:0]	R_ADDR;
 wire	[63:0]	W_DATA;
 wire	[7:0]	W_SPACE;
+wire	[31:0]	R_ADDR;
 
 assign W_ACTION        = PC_DATA [119:104];
 assign BEFORE_R_ADDR   = PC_DATA [119:32];
@@ -34,6 +33,8 @@ assign ENTER 	       = PC_DATA [15:0];
 assign W_SPACE	       = PC_DATA [87:80];
 assign DSP_DATA        = R_DATA;
 
+parameter [31:0] FAIL = 40'h46_4149_4C3E
+parameter [31:0] OK   = 40'h00_004F_4B3E
 
 always @(posedge CLK or posedge RST) begin
 	if (!RST) begin
@@ -46,7 +47,12 @@ always @(posedge CLK or posedge RST) begin
 	end
 	else if (BEFORE_R_ADDR = 88'h00_0000_0000_0000_0000_525F) begin
 		if (R_ADDR <= 16'h3038 && R_ADDR >= 16'h3030) begin
-
+			EN_READ <= 1;
+		end
+		else begin
+			DSP <= FAIL;
+		end
+	end
 
 	end
 end
