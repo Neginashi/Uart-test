2ec3c43686b9447220639fa43a77cd4a7078e013
Neginashi <52062701+Neginashi@users.noreply.github.com>
2022-05-24 14:35:41 +0900

    Update UART_PRO_TX.v

--- UART_PRO_TX.v
+++ UART_PRO_TX.v
@@ -1,59 +1,162 @@
 `timescale 1ns / 1ps
 
-module UART_ENC(
+module UART_PRO_TX(
 	input wire			CLK,
 	input wire			DATA_EN,
 	input wire [79:0]	DATA_IN,
 	input wire			FAIL,
 	input wire			OK,
 	input wire			STATE_R,
+	input wire			START,	
 
-	output  reg         CLK,
-    output  reg         TX_EN,
-    output  reg         START,
-    output  reg [7:0]   DATA_OUT
-
+	output  reg			START_OUT,
+	output  reg [7:0]	DATA_OUT
 	);
 
-	reg [2:0] 	STATE = {STATE_R, OK, FAIL};
+	reg [79:0] 	DATA_BUF;
 	reg [3:0] 	CNT_R;
-	reg [1:0] 	CNT_OK;
+	reg [2:0] 	CNT_OK;
 	reg [2:0] 	CNT_FAIL;
-	reg 		DONE;
+	reg [12:0] 	CNT_BIT;
+	reg [2:0]	STATE;
 	
+	// STATEs of STATE machine
+	parameter 		IDLE 		= 3'b001;
+	parameter 		STATE_READ 	= 3'b010;
+	parameter 		STATE_OK 	= 3'b011;
+	parameter 		STATE_FAIL 	= 3'b100;
+	parameter 		STOP	 	= 3'b101;
+
 	initial begin
-		DATA_IN 	<= 80'b0;
 		DATA_OUT 	<= 8'b0;
-		FAIL		<= 1'b0;
-		OK 			<= 1'b0;
-		STATE_R 	<= 1'b0;
-		STATE 		<= 3'b0;
+		CNT_R	 	<= 4'b0;
+		CNT_OK	 	<= 3'b0;
+		CNT_FAIL	<= 3'b0;
+		STATE 		<= IDLE;
+		START_OUT 	<= 1'b0;
+		CNT_BIT 	<= 13'b0;
 	end
 
 	always @(posedge CLK) begin
 		if (!DATA_EN) begin
-			DATA_IN 	<= 80'b0;
 			DATA_OUT 	<= 8'b0;
-			FAIL		<= 1'b0;
-			OK 			<= 1'b0;
-			STATE_R 	<= 1'b0;
+			CNT_R	 	<= 4'b0;
+			CNT_OK	 	<= 3'b0;
+			CNT_FAIL	<= 3'b0;
+			STATE 	 	<= IDLE;
+			START_OUT 	<= 1'b0;
+			CNT_BIT 	<= 13'b0;
 		end
 		else begin
 			case (STATE)
-				3'b100: begin
-					if (CNT_R <= 4'b1001) begin
-						DATA_OUT 	<= DATA_IN[79:72];
-						DATA_IN 	<= DATA_IN << 8;
-						CNT_R 		<= 4'b1;
-						if (CNT_R == 4'b1001) begin
-							CNT_R 	<= 4'b0;
-							DONE 	<= 2'b1;
+				default: begin
+					STATE <= IDLE;
+				end
+				
+				IDLE: begin
+					if (START) begin
+						DATA_BUF 	<= DATA_IN;
+						if (STATE_R) begin
+							STATE <= STATE_READ;
+						end
+						else if (OK) begin
+							STATE <= STATE_OK;
+						end
+						else if (FAIL) begin
+							STATE <= STATE_FAIL;
+						end
+						else begin
+							STATE <= IDLE;
 						end
 					end
+					else begin
+						DATA_OUT 	<= 8'b0;
+						CNT_R	 	<= 4'b0;
+						CNT_OK	 	<= 3'b0;
+						CNT_FAIL	<= 3'b0;
+						STATE  		<= IDLE;
+						START_OUT 	<= 1'b0;
+						CNT_BIT 	<= 13'b0;
+					end
 				end
-				3'b010: begin
 
+				STATE_READ: begin
+					if (CNT_R <= 4'b1011) begin
+						if (CNT_BIT == 13'b0) begin
+							START_OUT 	<= 1'b1;
+							DATA_OUT 	<= DATA_BUF[79:72];
+							DATA_BUF 	<= DATA_BUF << 8;
+							CNT_R 		<= CNT_R + 4'b1;
+							CNT_BIT 	<= 13'b1_0000_1110_1010;
+							if (CNT_R == 4'b1011) begin
+								CNT_R 		<= 4'b0;
+								STATE 		<= STOP;
+							end
+						end
+						else begin
+							CNT_BIT <= CNT_BIT - 13'b1;
+						end
+					end
+					else begin
+						STATE <= IDLE;
+					end
 				end
-				3'b001: begin
 
+				STATE_OK: begin
+					if (CNT_OK <= 3'b100) begin
+						if (CNT_BIT == 13'b0) begin
+							START_OUT 	<= 1'b1;
+							DATA_OUT 	<= DATA_BUF[79:72];
+							DATA_BUF 	<= DATA_BUF << 8;
+							CNT_OK 		<= CNT_OK + 3'b1;
+							CNT_BIT 	<= 13'b1_0000_1110_1010;
+							if (CNT_OK == 3'b100) begin
+								CNT_OK 		<= 2'b0;
+								STATE 		<= STOP;
+							end
+						end
+						else begin
+							CNT_BIT <= CNT_BIT - 13'b1;
+						end
+					end
+					else begin
+						STATE <= IDLE;
+					end
 				end
+
+				STATE_FAIL: begin
+					if (CNT_FAIL <= 3'b111) begin
+						if (CNT_BIT == 13'b0) begin
+							START_OUT 	<= 1'b1;
+							DATA_OUT 	<= DATA_BUF[79:72];
+							DATA_BUF 	<= DATA_BUF << 8;
+							CNT_FAIL 	<= CNT_FAIL + 3'b1;
+							CNT_BIT 	<= 13'b1_0000_1110_1010;
+							if (CNT_FAIL == 3'b111) begin
+								CNT_FAIL 	<= 2'b0;
+								STATE 		<= STOP;
+							end
+						end
+						else begin
+							CNT_BIT <= CNT_BIT - 13'b1;
+						end
+					end
+					else begin
+						STATE <= IDLE;
+					end
+				end
+
+				STOP: begin
+					DATA_OUT 	<= 8'b0;
+					CNT_R	 	<= 4'b0;
+					CNT_OK	 	<= 3'b0;
+					CNT_FAIL	<= 3'b0;
+					STATE  		<= IDLE;
+					START_OUT 	<= 1'b0;
+					CNT_BIT 	<= 13'b0;
+				end
+			endcase
+		end
+	end
+
+endmodule
