d0e9ed1c31dceba7ce3fba260268994e23cd0b58
Neginashi <52062701+Neginashi@users.noreply.github.com>
2022-05-24 14:36:54 +0900

    Update URAT_ENC.v

--- URAT_ENC.v
+++ URAT_ENC.v
@@ -7,39 +7,73 @@ module UART_ENC(
 	input 			FAIL,
 	input 			OK,
 	input			STATE_R,
+	input 			START,
+
 
 	output reg [79:0]	DATA_OUT,
 	output reg			FAIL_OUT,
 	output reg			OK_OUT,
-	output reg			STATE_R_OUT
+	output reg			STATE_R_OUT,
+	output reg 			DONE
 	);
-
-	reg [2:0] STATE = {STATE_R, OK, FAIL};
+	
+	reg [2:0] 	STATE;
+	reg [31:0] 	DATA_BUF;
+	
+	parameter IDLE 			= 3'b001;
+	parameter STATE_DATA 	= 3'b010;
+	parameter STATE_FAIL 	= 3'b011;
+	parameter STATE_OK 		= 3'b100;
+	parameter STOP 			= 3'b101;
 
 	initial begin
-		DATA_IN 	<= 32'b0;
 		DATA_OUT 	<= 80'b0;
-		FAIL		<= 1'b0;
-		OK 			<= 1'b0;
-		STATE_R 	<= 1'b0;
-		STATE 		<= 3'b0;
+		FAIL_OUT	<= 1'b0;
+		OK_OUT 		<= 1'b0;
+		STATE_R_OUT <= 1'b0;
+		STATE 		<= 3'b001;
+		DATA_BUF 	<= 32'b0;
 	end
-
-	reg [2:0] STATE = {STATE_R, OK, FAIL};
 
 	always @(posedge CLK) begin
 		if (!DATA_EN) begin
-			// reset
-			DATA_IN 	<= 32'b0;
+			STATE 		<= IDLE;
 			DATA_OUT 	<= 80'b0;
-			FAIL		<= 1'b0;
-			OK 			<= 1'b0;
-			STATE_R 	<= 1'b0;
+			FAIL_OUT	<= 1'b0;
+			OK_OUT 		<= 1'b0;
+			STATE_R_OUT <= 1'b0;
+			STATE 		<= 3'b001;
+			DONE 		<= 1'b0;
+			DATA_BUF 	<= 32'b0;
 		end
 		else begin
 			case (STATE)
-				3'b100: begin
-					case (DATA_IN[3:0])
+				IDLE: begin
+					if (START) begin
+						DATA_BUF <= DATA_IN;
+						if (STATE_R) begin
+							STATE <= STATE_DATA;
+						end
+						else if (OK) begin
+							STATE <= STATE_OK;
+						end
+						else if (FAIL) begin
+							STATE <= STATE_FAIL;
+						end
+						else begin
+							STATE <= IDLE;
+							DATA_OUT 	<= 80'b0;
+							FAIL_OUT	<= 1'b0;
+							OK_OUT 		<= 1'b0;
+							STATE_R_OUT <= 1'b0;
+							DONE 		<= 1'b0;
+							DATA_BUF 	<= 32'b0;
+						end
+					end
+				end
+
+				STATE_DATA: begin
+					case (DATA_BUF[3:0])
 						4'h0: DATA_OUT[23:16] <= 8'h30;
 						4'h1: DATA_OUT[23:16] <= 8'h31;
 						4'h2: DATA_OUT[23:16] <= 8'h32;
@@ -57,7 +91,7 @@ module UART_ENC(
 						4'hE: DATA_OUT[23:16] <= 8'h45;
 						4'hF: DATA_OUT[23:16] <= 8'h46;
 					endcase
-					case (DATA_IN[7:4])
+					case (DATA_BUF[7:4])
 						4'h0: DATA_OUT[31:24] <= 8'h30;
 						4'h1: DATA_OUT[31:24] <= 8'h31;
 						4'h2: DATA_OUT[31:24] <= 8'h32;
@@ -75,7 +109,7 @@ module UART_ENC(
 						4'hE: DATA_OUT[31:24] <= 8'h45;
 						4'hF: DATA_OUT[31:24] <= 8'h46;
 					endcase
-					case (DATA_IN[11:8])
+					case (DATA_BUF[11:8])
 						4'h0: DATA_OUT[39:32] <= 8'h30;
 						4'h1: DATA_OUT[39:32] <= 8'h31;
 						4'h2: DATA_OUT[39:32] <= 8'h32;
@@ -93,7 +127,7 @@ module UART_ENC(
 						4'hE: DATA_OUT[39:32] <= 8'h45;
 						4'hF: DATA_OUT[39:32] <= 8'h46;
 					endcase
-					case (DATA_IN[15:12])
+					case (DATA_BUF[15:12])
 						4'h0: DATA_OUT[47:40] <= 8'h30;
 						4'h1: DATA_OUT[47:40] <= 8'h31;
 						4'h2: DATA_OUT[47:40] <= 8'h32;
@@ -111,7 +145,7 @@ module UART_ENC(
 						4'hE: DATA_OUT[47:40] <= 8'h45;
 						4'hF: DATA_OUT[47:40] <= 8'h46;
 					endcase
-					case (DATA_IN[19:16])
+					case (DATA_BUF[19:16])
 						4'h0: DATA_OUT[55:48] <= 8'h30;
 						4'h1: DATA_OUT[55:48] <= 8'h31;
 						4'h2: DATA_OUT[55:48] <= 8'h32;
@@ -129,7 +163,7 @@ module UART_ENC(
 						4'hE: DATA_OUT[55:48] <= 8'h45;
 						4'hF: DATA_OUT[55:48] <= 8'h46;
 					endcase
-					case (DATA_IN[23:20])
+					case (DATA_BUF[23:20])
 						4'h0: DATA_OUT[63:56] <= 8'h30;
 						4'h1: DATA_OUT[63:56] <= 8'h31;
 						4'h2: DATA_OUT[63:56] <= 8'h32;
@@ -147,7 +181,7 @@ module UART_ENC(
 						4'hE: DATA_OUT[63:56] <= 8'h45;
 						4'hF: DATA_OUT[63:56] <= 8'h46;
 					endcase
-					case (DATA_IN[27:24])
+					case (DATA_BUF[27:24])
 						4'h0: DATA_OUT[71:64] <= 8'h30;
 						4'h1: DATA_OUT[71:64] <= 8'h31;
 						4'h2: DATA_OUT[71:64] <= 8'h32;
@@ -165,7 +199,7 @@ module UART_ENC(
 						4'hE: DATA_OUT[71:64] <= 8'h45;
 						4'hF: DATA_OUT[71:64] <= 8'h46;
 					endcase
-					case (DATA_IN[31:28])
+					case (DATA_BUF[31:28])
 						4'h0: DATA_OUT[79:72] <= 8'h30;
 						4'h1: DATA_OUT[79:72] <= 8'h31;
 						4'h2: DATA_OUT[79:72] <= 8'h32;
@@ -183,20 +217,48 @@ module UART_ENC(
 						4'hE: DATA_OUT[79:72] <= 8'h45;
 						4'hF: DATA_OUT[79:72] <= 8'h46;
 					endcase
-					DATA_OUT[15:0] <= 16'h0A3E;
+					DATA_OUT[15:0] 	<= 16'h0A3E;
+					DONE 			<= 1'b1;
+					STATE 			<= STOP;
+					STATE_R_OUT 	<= 1'b1;
+					OK_OUT 			<= 1'b0;
+					FAIL_OUT 		<= 1'b0;
 				end
-				3'b010: begin
-					DATA_OUT[79:64] <= 16'h4F4B;
-					DATA_OUT[63:48] <= 16'h0A3E;
-				end
-				3'b001: begin
+
+				STATE_FAIL: begin
 					DATA_OUT[79:48] <= 32'h4641_494C;
 					DATA_OUT[47:32] <= 16'h0A3E;
+					DONE 			<= 1'b1;
+					STATE 			<= STOP;
+					STATE_R_OUT 	<= 1'b0;
+					OK_OUT 			<= 1'b0;
+					FAIL_OUT 		<= 1'b1;
+				end
+
+				STATE_OK: begin
+					DATA_OUT[79:64] <= 16'h4F4B;
+					DATA_OUT[63:48] <= 16'h0A3E;
+					DONE 			<= 1'b1;
+					STATE 			<= STOP;
+					STATE_R_OUT 	<= 1'b0;
+					OK_OUT 			<= 1'b1;
+					FAIL_OUT 		<= 1'b0;
+				end
+
+				STOP: begin
+					STATE <= IDLE;
+					DATA_OUT 	<= 80'b0;
+					FAIL_OUT	<= 1'b0;
+					OK_OUT 		<= 1'b0;
+					STATE_R_OUT <= 1'b0;
+					DONE 		<= 1'b0;
+					DATA_BUF 	<= 32'b0;
+				end
+
+				default: begin
+					STATE <= IDLE;
 				end
 			endcase
-			STATE_R_OUT <= STATE_R;
-			OK_OUT 		<= OK;
-			FAIL_OUT 	<= FAIL;
 		end
 	end
 
